e081798755a17cbcdfd9df27dae52941d84204bc
Lu Ken <bluewish.ken.lu@gmail.com>
2017-09-17 01:28:00 +0800

    refine the rosinstall files

--- init-repo.sh
+++ init-repo.sh
@@ -4,7 +4,8 @@ WS_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p ${WS_DIR}/src/
 wstool init -j8 src repos/kinetic-desktop-wet.rosinstall
-wstool merge -t src repos/kobuki.rosinstall
+wstool merge -t src repos/yujinrobot.rosinstall
 wstool merge -t src repos/rocon.rosinstall
 wstool merge -t src repos/turtlebot.rosinstall
+wstool merge -t src repos/misc.rosinstall
 wstool update -j8 -t src
