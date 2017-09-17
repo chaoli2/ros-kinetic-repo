de923a513c51d4885503ef3c7e1449328e243006
Lu, Ken <bluewish.ken.lu@gmail.com>
2017-09-17 11:35:20 +0800

    for foundation kinetic desktop repo, use git method instead of tar package

--- init-repo.sh
+++ init-repo.sh
@@ -3,7 +3,7 @@
 WS_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p ${WS_DIR}/src/
-wstool init -j8 src repos/kinetic-desktop-wet.rosinstall
+wstool init -j8 src repos/kinetic-desktop.rosinstall
 wstool merge -t src repos/yujinrobot.rosinstall
 wstool merge -t src repos/rocon.rosinstall
 wstool merge -t src repos/turtlebot.rosinstall
